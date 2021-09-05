47c4d45b8cfa9b416369b42dc007b0d57e29f423
Bekhzod <bexzod9834@gmail.com>
2021-09-05 23:34:29 +0900

    added some functions

--- python_basic/test.py
+++ python_basic/test.py
@@ -1,4 +1,4 @@
-import math
+'''import math
 a=10
 b=5
 c=7
@@ -6,3 +6,25 @@ p=(a+b+c)/2
 
 area = math.sqrt(p*(p-a)*(p-b)*(p-c))
 print(area)
+a = True
+b = False
+print(type(a)) 
+z = None
+print(z)
+string = 'hello my name is Bekhzod'
+print(len(string))
+print(string.count('l'))
+print(string.capitalize())
+upper_cased = string.upper()
+lower_cased = string.lower()
+print(upper_cased.isupper())
+print(lower_cased.islower())
+print(string.islower())'''
+empty_string = ''
+if not empty_string:
+    print('not empty')
+else:
+        print('empty')
+h = 'hello'
+print(h.startswith('hel'))
+print(h.endswith('os'))
