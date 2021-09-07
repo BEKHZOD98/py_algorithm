ca41abd97e0de9f7ae15016c73ae2793460033bf
Bekhzod98 <bexzod9834@gmail.com>
2021-09-07 10:54:39 +0900

    'hcf'

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-print('hello')
-print('hello')
+def gcd(a,b):
+    while (b !=0):
+        temp = b
+        b = a% b
+        a = temp
+    return a
+
+print("HCD(60, 28) = " ,gcd(60,28))
+'''
+def gcd(a,b):
+    while (b !=0):
+        temp = b
+        b = a% b
+        a = temp
+    return a
+'''
