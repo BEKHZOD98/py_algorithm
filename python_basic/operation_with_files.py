046adafa1e745a35e79a7026c3b2657642081b87
BEKHZOD98 <bexzod9834@gmail.com>
2021-09-09 20:00:21 +0900

    added some functions and gitignore file

--- python_basic/operation_with_files.py
+++ python_basic/operation_with_files.py
@@ -8,13 +8,36 @@ dirpath = os.getcwd()
 f = open("demo_pyfile.txt",'w')
 f.write('1) I have deleted the content \n2) I have deleted the content ')
 f.close()
+f = open("demo_pyfile.txt","r")
+datapy = f.read()
+print('this is datapy: ',datapy)
 
+
+#open , write and read second file
+second_file=open('demo2_pyfile.txt','w')
+second_file.write('1)Abbos\n2)Kabir\n3)Malik')
+second_file.close()
+second_file = open('demo2_pyfile.txt','r')
+data = second_file.read()
+print(data)
+
+second_file = open('demo2_pyfile.txt','r')
+data_sec_lines = second_file.readlines()
+print(data_sec_lines)
+second_file.close()
 #open and read the file after the appending
+
 f = open('demo_pyfile.txt','r')
 #print(f.read())
+'''
 data = f.read()
 f.seek(0)
-print(data)
+print(data)'''
 lines = f.readlines()
 print(lines)
 print(type(lines))
+print(len(lines))
+
+
+#delete folder or file
+os.remove('demo2_pyfile.txt') 
