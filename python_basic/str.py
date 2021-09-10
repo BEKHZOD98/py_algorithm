648ea71875c3dda25f28ca0b9b73ee96b59c51c7
BEKHZOD98 <bexzod9834@gmail.com>
2021-09-10 23:10:00 +0900

    modified  some files

--- python_basic/str.py
+++ python_basic/str.py
@@ -1,4 +1,39 @@
-import sys
+'''[import sys
 system = sys.getdefaultencoding()
 print(system)
 print(ord('a'))
+s = 'hello'
+enc_ascii = s.encode('ascii')
+print(enc_ascii)
+print(type(enc_ascii))
+print("I'm a student and I'll become a \"strong\" programmer")
+print("C:\\Users\\GoodStudent")
+x = "My name is Agent Smith"
+
+coffe = int(coffeCol = input("Enter the num of cups: "))
+bonus = int(coffe /6)
+print(bonus)
+import math
+x1 = int(input('enter x1: '))
+y1 = int(input('enter x1: '))
+x2 = int(input('enter x1: '))
+y2 = int(input('enter x1: '))
+distance = math.sqrt((x1-x2)**2  + (y1 -y2)**2)
+print("{:.3f}".format(distance),sep='')'''
+chicken = int(input('how many chicken?'))
+cows = int(input('how many cows?'))
+pigs = int(input('how many pigs?'))
+total = (chicken*2)+(cows*4)+(pigs*4)
+print("total legs animal: ",total)
+
+
+
+
+
+
+
+
+
+
+
+
