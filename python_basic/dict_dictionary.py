46ede32dacbb3dfe1de6a4ed9023d636ab868489
BEKHZOD98 <bexzod9834@gmail.com>
2021-09-14 01:15:06 +0900

    added get(), pop(), update() and list functions

--- python_basic/dict_dictionary.py
+++ python_basic/dict_dictionary.py
@@ -14,3 +14,34 @@ print(players)
 top1 = players['Carlsen']
 print('Top chess player is: ',top1) #agarda bu usulda yozilsa consolega Carlsenni faqat value score chiqadi
       #qosh tirnoni ichida {top1} qilib ham yozsa boladi
+#Bunda tashqari consolega chiqarishni 2chi yoli get() functionidan foydalanish dir
+print(players.get('Ding'))
+players['So'] = 2760 #qilib dictga yana bitta key va value ni qosha olamiza
+print(players)
+players.pop('Giri') # shu usulda girini delete qilamiza
+del players['So'] #orqali delete qisa boladi
+print(players)
+
+
+
+
+#The method update() adds dictionary dict2's key-values pairs in to dict. 
+student = {
+    'Name': 'Habe', 'Age': 7
+    }
+Human = {
+    'Sex': 'Male'
+}
+student.update(Human)
+print(student)
+
+#Dict ni faqat gina Key larini con.log qilish un keys() func ni ishlatamiza
+keys = players.keys()
+print(type(keys),keys)
+
+
+
+#Dict ni key larini list functioniga solgan holda chiqarish ham qulay
+lis = list(players.keys())
+print(type(lis),lis) #Bunday holda lis variable type list boladi
+
