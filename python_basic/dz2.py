fd04ec2b7300a80eac7c5000f3024673aba30a21
BEKHZOD98 <bexzod9834@gmail.com>
2021-10-13 19:39:48 +0900

    'blackjack'

--- python_basic/dz2.py
+++ python_basic/dz2.py
@@ -24,7 +24,7 @@ for x in second_list:
     if x % 2 == 0:
         join_list.append(x)
 
-print(f'this is the new list: {join_list}')'''
+print(f'this is the new list: {join_list}')
 
 #Merge list problem solved with list comperhancion
 first_list = [1,2,3,4,5,6]
@@ -34,4 +34,26 @@ odds = [x for x in first_list if x % 2 !=0]
 evens = [x for x in second_list if x % 2 ==0]
 
 merge_list = odds + evens
-print(f'Merged list: {merge_list}')
+print(f'Merged list: {merge_list}')'''
+
+#problem black jack
+
+cards = {
+    2: 1,
+    3: 1,
+    4: 1,
+    5: 1,
+    6: 1,
+    7:0,
+    8:0,
+    9:0,
+    10: -1,
+    'J': -1,
+    'Q': -1,
+    'K': -1
+}
+current_hand = [2,3,4,10,'Q',5]
+
+cards_sum = sum([cards[x] for x in current_hand ])
+print(cards_sum)
+
