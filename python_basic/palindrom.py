41955972abae42eefe6b3912a829d863e3034819
BEKHZOD98 <bexzod9834@gmail.com>
2021-10-15 22:07:33 +0900

    palindrome

---
+++ python_basic/palindrom.py
@@ -0,0 +1,12 @@
+number = int(input('Enter a number'))
+
+reversed_number = 0
+tmp_original = number
+
+while tmp_original > 0:
+    reversed_number = (reversed_number * 10) + tmp_original % 10
+    tmp_original = tmp_original // 10
+if number == reversed_number:
+    print('This is Pallindrome')
+else: 
+    print('No Pallindrome')
