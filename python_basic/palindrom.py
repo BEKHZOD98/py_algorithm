02eeebc8634fe96bb39b4bd93bbe53c4883ab440
BEKHZOD98 <bexzod9834@gmail.com>
2021-10-16 02:03:59 +0900

    mini guess game

--- python_basic/palindrom.py
+++ python_basic/palindrom.py
@@ -1,7 +1,7 @@
 number = int(input('Enter a number'))
 
 reversed_number = 0
-tmp_original = number
+tmp_original = number#121
 
 while tmp_original > 0:
     reversed_number = (reversed_number * 10) + tmp_original % 10
@@ -10,3 +10,31 @@ if number == reversed_number:
     print('This is Pallindrome')
 else: 
     print('No Pallindrome')
+
+
+s = input('Enter the value: ')
+
+reverse = s[::-1]
+
+if(s==reverse):
+    print('Yes it is palindrome')
+else: 
+    print('No it si no Palindrome')
+
+#num = [0,1,2,3,4,5,6,7,8,9]
+#print(num[:-1:-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
